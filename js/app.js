4146ced03dbcabcd69d0e2d0fa5b7da6a3275ba3
RACHID ADOUL <ra.adoul@gmail.com>
2014-12-02 19:28:23 +0000

    Ionic framework added, life is so complicated, plzz halp me next time

--- js/app.js
+++ js/app.js
@@ -1,20 +1,54 @@
-angular.module('lolsmartpick', ['ngTouch', 'ngSanitize', 'ngRoute'])
-.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/pick', {
-        templateUrl: 'views/pickChamps.html',
-        controller: 'accueilController'
-      }).
-      when('/champs', {
-        templateUrl: 'views/listChamp.html',
-        controller: 'listChampController'
-      }).
-      otherwise({
-        redirectTo: '/pick'
-      });
-  }])
-.controller('accueilController', ['$scope', '$sce', '$location', function($scope, $sce, $location) {
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('lolsmartpick', ['ionic', 'ngRoute', 'ngTouch'])
+
+.config(function ($compileProvider){
+  // Set the whitelist for certain URLs just to be safe
+  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+})
+
+.config(function($routeProvider, $locationProvider) {
+  // Set up the initial routes that our app will respond to.
+  // These are then tied up to our nav router which animates and
+  // updates a navigation bar
+  $routeProvider.when('/welcome', {
+    templateUrl: 'templates/welcome.html',
+    controller: 'welcomeController'
+  });
+
+  $routeProvider.when('/pick', {
+    templateUrl: 'templates/pickChamps.html',
+    controller: 'pickController'
+  });
+  
+  $routeProvider.when('/champs', {
+    templateUrl: 'templates/listChamp.html',
+    controller: 'listChampController'
+  });
+
+  // if none of the above routes are met, use this fallback
+  // which executes the 'IntroCtrl' controller (controllers.js)
+  $routeProvider.otherwise({
+    redirectTo: '/welcome'
+  });
+})
+  
+.controller('welcomeController', function($scope, $location) {
+	$scope.goToPickAssistance = function(){
+		$location.path("/pick");
+	}
+})
+
+.controller('listChampController', function($scope, $location) {
+	//todo
+})
+
+.controller('pickController', function($scope, $location) {
 	var DEFAULT_POSTE = "N<br/>O<br/>N<br/>E";
 	$scope.alliesPoste = [
 		{"name" : "T<br/>O<br/>P"},
@@ -66,4 +100,4 @@ angular.module('lolsmartpick', ['ngTouch', 'ngSanitize', 'ngRoute'])
 	$scope.selectChamp = function(ally){
 		$location.path("/champs");
 	}
-}]); 
+});
